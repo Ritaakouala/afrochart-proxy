171b50c1ef78221bd9a9586c638c222b768a07c9
Rita akouala <akoualarita2021@gmail.com>
2026-08-30 00:02:49 +0000

    Modify handler to fetch chart by genre ID

--- api/deezer.js
+++ api/deezer.js
@@ -1,5 +1,11 @@
-export default async function handler(req, res) { res.setHeader('Access-Control-Allow-Origin', '*');
-  try { const response = await fetch('https://api.deezer.com/chart'); const data = await response.json(); 
-       res.status(200).json(data); } catch (error) { res.status(500).json({ error: 'Erreur lors de la récupération des données' }); } }
-
-
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const genreId = req.query.genre || 0; // 0 = classement mondial toutes musiques
+  try {
+    const response = await fetch(`https://api.deezer.com/chart/${genreId}`);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la récupération des données' });
+  }
+}
