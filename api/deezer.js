95604f05f1df8edd4d7e3c32cd797014ce3580fd
Rita akouala <akoualarita2021@gmail.com>
2026-08-30 00:47:45 +0000

    Update deezer.js

--- api/deezer.js
+++ api/deezer.js
@@ -3,11 +3,17 @@ export default async function handler(req, res) {
   const genreId = req.query.genre || 0;
   const limit = req.query.limit || 10;
   const albumId = req.query.albumId;
+  const search = req.query.search;
+  const playlistId = req.query.playlistId;
 
   try {
     let url;
-    if (albumId) {
+    if (search) {
+      url = `https://api.deezer.com/search?q=${encodeURIComponent(search)}&limit=${limit}`;
+    } else if (albumId) {
       url = `https://api.deezer.com/album/${albumId}/tracks`;
+    } else if (playlistId) {
+      url = `https://api.deezer.com/playlist/${playlistId}/tracks?limit=${limit}`;
     } else {
       url = `https://api.deezer.com/chart/${genreId}?limit=${limit}`;
     }
