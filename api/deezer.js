a75cc1a405f004cbf105ae1ce5976d37c06114b0
Rita akouala <akoualarita2021@gmail.com>
2026-08-30 00:36:25 +0000

    Add limit and albumId support to Deezer API handler

--- api/deezer.js
+++ api/deezer.js
@@ -1,8 +1,17 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   const genreId = req.query.genre || 0;
+  const limit = req.query.limit || 10;
+  const albumId = req.query.albumId;
+
   try {
-    const response = await fetch(`https://api.deezer.com/chart/${genreId}`);
+    let url;
+    if (albumId) {
+      url = `https://api.deezer.com/album/${albumId}/tracks`;
+    } else {
+      url = `https://api.deezer.com/chart/${genreId}?limit=${limit}`;
+    }
+    const response = await fetch(url);
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
